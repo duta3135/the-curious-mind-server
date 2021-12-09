253be0f418d4c98fcc0e00562f2fe73009092ad2
duta3135 <dutasetyawan1012@gmail.com>
2021-12-09 14:15:28 +0700

    commit -m "finally connected to database"

--- app.js
+++ app.js
@@ -3,6 +3,19 @@ const cors = require('cors')
 const app = express()
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
+const db = require('./app/models/')
+db.mongoose
+    .connect(db.url, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    })
+    .then(()=>{
+        console.log('connected to db')
+    })
+    .catch((err)=>{
+        console.log(err)
+        process.exit()
+    })
 app.get('/', (req,res)=>{
     res.send("you're on home")
 })
