a87c870e80988c084bcdeea13e4e71b20abc14d5
duta3135 <dutasetyawan1012@gmail.com>
2021-12-10 14:45:06 +0700

    commit

--- routes/articles.js
+++ routes/articles.js
@@ -1,6 +1,7 @@
 const Article = require('../models/Article')
 const mongoose = require('mongoose')
 const router = require('express').Router()
+//post an article
 router.post('/', (req,res)=>{
     const article = new Article({
     _id: new mongoose.Types.ObjectId(),
@@ -11,15 +12,26 @@ router.post('/', (req,res)=>{
     content: req.body.content,
     published: req.body.published,
     time: Date.now()
-})
-article.save()  
-    .then(()=>{
-        res.status(200).send('posted an article')       
-    }) 
-    .catch(err=>{
-        res.status(500).json({
-            message: err
-        })
     })
+    article.save()  
+        .then(()=>{
+            res.status(200).send('posted an article')       
+        }) 
+        .catch(err=>{
+            res.status(500).json({
+                message: err
+            })
+        })
+    }
+)
+//get all articles
+router.get('/', async (req, res)=>{
+    try{
+        const articles = await Article.find()
+        res.send(articles)
+    }
+    catch(err){
+        console.log(err)
+    }
 })
 module.exports = router
