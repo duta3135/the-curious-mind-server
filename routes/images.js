4303911f5bcff1c22f1839d783331a4908b43277
duta3135 <dutasetyawan1012@gmail.com>
2022-01-14 15:00:00 +0700

    commit

--- routes/images.js
+++ routes/images.js
@@ -10,27 +10,32 @@ router.post('/', (req, res) => {
     if(!req.files){
         return res.status(400).json({message: 'no files'})
     }
-    const chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
-    var id;
-    for (var i = 0; i <= 4; i++) {
-        var randomNumber = Math.floor(Math.random() * chars.length);
-        var rawTxt;
-        rawTxt += chars.substring(randomNumber, randomNumber +1);
-        id = rawTxt.replace('undefined', '')
-       }
-     
-    // console.log(req.files)
-    const file= req.files.image
-    const uploadPath= `${__dirname}/../uploads/${file.name}`
-    file.mv(uploadPath, function(err){
-        if (err){
-            return res.status(500).send(err)
-        }
-    })
-    cloudinary.v2.uploader.upload(uploadPath, {exif: true, public_id: `${file.name}${id}`}, function(err, response){
-        if(err) res.send(err)
-        res.send(response)
-        fs.unlinkSync(uploadPath)
-    });
+    try{
+        const chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+        var id;
+        for (var i = 0; i <= 4; i++) {
+            var randomNumber = Math.floor(Math.random() * chars.length);
+            var rawTxt;
+            rawTxt += chars.substring(randomNumber, randomNumber +1);
+            id = rawTxt.replace('undefined', '')
+           }
+         
+        // console.log(req.files)
+        const file= req.files.image
+        const uploadPath= `${__dirname}/../uploads/${file.name}`
+        file.mv(uploadPath, function(err){
+            if (err){
+                return res.status(500).send(err)
+            }
+        })
+        cloudinary.v2.uploader.upload(uploadPath, {exif: true, public_id: `${file.name}${id}`}, function(err, response){
+            if(err) res.send(err)
+            res.status(200).send(response)
+            fs.unlinkSync(uploadPath)
+        });
+    }
+    catch(err){
+        res.status(500).json({message: err})
+    }
 })
 module.exports = router
